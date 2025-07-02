7d75e0914b320a356e8886b31eae56428503aeb4
Sej <sej@mac.lan>
2025-07-02 13:09:58 -0500

    Remove backend/config.py from repository

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY", "AIzaSyA3Jj-m8ih28GTvlte0-xb5CNGwTbRW5jY")
-    VECTOR_SEARCH_URL = "https://www.ac0.cloudadvocacyorg.joonix.net/api/query"
-    APP_NAME = "shop_concierge_app"
-    USER_ID = "user_1"
-
-config = Config()
